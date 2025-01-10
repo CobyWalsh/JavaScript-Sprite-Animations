6e7f273544b8117f1b9c1ce18442fd2cbf1b25eb
Coby Walsh <cobywalsh7@yahoo.com>
2025-01-09 18:17:41 -0700

    created index.html

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
-
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
+const playerState = "run";
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 4;
 let gameFrame = 0;
 let staggerFrames = 5;
 const spriteAnimations = [];
@@ -20,14 +18,61 @@ const animationStates = [
     {
         name: 'jump',
         frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },
+    {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'ko',
+        frames: 12,
+    },
+    {
+        name: 'getHit',
+        frames: 7,
+    },
+];
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
     }
-]
+    for (let j = 0; j < state.frames; j++) {
+        let postionX = j * spriteWidth;
+        let postionY = index * spriteHeight
+        frames.loc.push({x: postionX, y: postionY});
+    }
+    spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
+
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let postion = Math.floor(gameFrame/staggerFrames) % 11;
+    let postion = Math.floor(gameFrame/staggerFrames) % spriteAnimations["idle"].loc.length;
     frameX = spriteWidth * postion;
-    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, 
+        spriteHeight, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 
 
 
